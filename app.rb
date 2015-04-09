0bbb8b256a33223028a7a700b88e638c04093975
htmmc <hitomi.mzgk@gmail.com>
2015-04-09 14:49:29 -0400

    removed tab

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'json'
 
 get '/code' do 
 	content_type :json
-	str = File.readlines('app.rb').flat_map{|i| i.split("\n")}.join(", ")
+	str = File.readlines('app.rb').flat_map{|i| i.split(/[\n\t]/)}.join(", ")
 	file = {"codes" => str}
 	JSON.pretty_generate(file)
 end 
