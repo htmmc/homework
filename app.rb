cbf39ac1a509c96e88e07dda00c586eb169f255a
htmmc <hitomi.mzgk@gmail.com>
2015-04-09 14:35:20 -0400

    made changes to print the very file that is being edited

--- app.rb
+++ app.rb
@@ -3,5 +3,7 @@ require 'json'
 
 get '/code' do 
 	content_type :json
-	file = File.read('app.json')
+	str = File.readlines('app.rb').flat_map{|i| i.split("\n")}.join(", ")
+	file = {"codes" => str}
+	JSON.pretty_generate(file)
 end 
